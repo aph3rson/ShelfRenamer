3a440d41a26f28978aabbc27f43540089b42f64e
Ian Williams <aph3rson@gmail.com>
2022-01-08 14:42:50 -0800

    Add debug helpers, make patch detect other gizmos
    Detects gizmo by name, not by thingClass.
    
    Works on emipa606/ShelfRenamer#1

--- Source/ShelfRenamer/Patch_Building_Storage_GetGizmos.cs
+++ Source/ShelfRenamer/Patch_Building_Storage_GetGizmos.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using RimWorld;
@@ -12,20 +13,56 @@ namespace ShelfRenamer
     [HarmonyPatch("GetGizmos")]
     public static class Patch_Building_Storage_GetGizmos
     {
+        // List of already-seen thingClasses
+        private static List<Type> alreadySeen = new List<Type>();
+        
+        // Debug logging method
+        private static void LogNotPatching(Building_Storage thing, string reason)
+        {
+            Type thingClass = thing.def.thingClass;
+            if (!alreadySeen.Contains(thingClass))
+            {
+                alreadySeen.Add(thingClass);
+                ShelfRenamer.Instance.Log(
+                    $"Not patching gizmos for {thing.def.thingClass.Name} because {reason}."
+                );
+            }
+        }
         public static void Postfix(Building_Storage __instance, ref IEnumerable<Gizmo> __result)
         {
             // ShelfRenamer.Instance.Log("Gizmoing " + __instance.def.thingClass.Name);
 
             // RimFridge already has its own renamer.
-            if (__instance.def.thingClass.Name == "Building_Refrigerator" ||
-                __instance.def.thingClass.Name == "RimFridge_Building")
+            // if (__instance.def.thingClass.Name == "Building_Refrigerator" ||
+            //     __instance.def.thingClass.Name == "RimFridge_Building")
+            // {
+            //     return;
+            // }
+            
+            // Loop through existing Gizmos
+            foreach (Gizmo giz in __result)
             {
-                return;
+                // If the gizmo is a command...
+                if (giz is Command)
+                {
+                    Command command = (Command)giz;
+                    // ...and is labeled "Rename":
+                    if (command.defaultLabel == "Rename" || // covers untranslated strings
+                        command.defaultLabel == "Rename".Translate())
+                    {
+                        // Log to the console that we're not patching this,
+                        LogNotPatching(__instance, "has gizmo already");
+                        // and skip adding a gizmo
+                        return;
+                    }
+                    
+                }
             }
 
             // If it has a user-accessible storage tab, then allow renaming.
             if (!__instance.StorageTabVisible)
             {
+                LogNotPatching(__instance, "no storage tab");
                 return;
             }
 
