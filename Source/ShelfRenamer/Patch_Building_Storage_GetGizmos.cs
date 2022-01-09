503957dc449e0e707294e8196ba500691d4f39c8
Ian Williams <aph3rson@gmail.com>
2022-01-08 18:47:13 -0800

    Improve detection of overloaded GetGizmos
    Properly detects method equality and overloads.
    Works with PRF storage items, detects RimFridge patches.
    
    Works on emipa606/ShelfRenamer#1

--- Source/ShelfRenamer/Patch_Building_Storage_GetGizmos.cs
+++ Source/ShelfRenamer/Patch_Building_Storage_GetGizmos.cs
@@ -13,23 +13,22 @@ namespace ShelfRenamer
     [HarmonyPatch]
     public static class Patch_Building_Storage_GetGizmos
     {
-        // List of already-seen thingClasses and reasons
-        private static List<(Type, String)> alreadySeen = new List<(Type, String)>();
+        // List of already-seen thingClasses, decisions, and reasons
+        private static List<(Type, bool, String)> alreadySeen = new List<(Type, bool, String)>();
         
         // List of subclasses with their own GetGizmos
         // We'll short-circuit the base postfix if they're in this list.
         private static List<Type> customGetGizmos = new List<Type>();
         
         // Debug logging method
-        private static void LogNotPatching(Building_Storage thing, string reason)
+        private static void LogOnlyOnce(Type type, bool willPatch, string reason)
         {
-            Type thingClass = thing.def.thingClass;
-            (Type, String) index = (thingClass, reason);
+            (Type, bool, String) index = (type, willPatch, reason);
             if (!alreadySeen.Contains(index))
             {
                 alreadySeen.Add(index);
                 ShelfRenamer.Instance.Log(
-                    $"Not patching gizmos for {thing.def.thingClass.Name} because {reason}."
+                    $"Patch gizmos: {willPatch} for {type} because {reason}."
                 );
             }
         }
@@ -43,18 +42,20 @@ namespace ShelfRenamer
             var baseGetGizmos = typeof(Building_Storage).GetMethod("GetGizmos");
             ret.Add(baseGetGizmos);
             
-            // Find all subclasses of GetGizmo
+            // Find all subclasses of Building_Storage
             var subclasses =
                 from assembly in AppDomain.CurrentDomain.GetAssemblies()
                 from type in assembly.GetTypes()
                 where type.IsSubclassOf(typeof(Building_Storage))
                 select type;
+            ShelfRenamer.Instance.Log(
+                $"{subclasses.Count()} subclasses of Building_Storage: {String.Join(", ", subclasses)}");
 
             // Find methods which are not the base method
             var subsGetGizmos =
                 from subclass in subclasses
                 let subGetGizmo = subclass.GetMethod("GetGizmos")
-                where !subGetGizmo.Equals(baseGetGizmos)
+                where !subGetGizmo.MethodHandle.Equals(baseGetGizmos.MethodHandle)
                 select subGetGizmo;
             ret.AddRange(subsGetGizmos);
             
@@ -71,20 +72,11 @@ namespace ShelfRenamer
         }
         public static void Postfix(Building_Storage __instance, ref IEnumerable<Gizmo> __result, MethodBase __originalMethod)
         {
-            // ShelfRenamer.Instance.Log("Gizmoing " + __instance.def.thingClass.Name);
-
-            // RimFridge already has its own renamer.
-            // if (__instance.def.thingClass.Name == "Building_Refrigerator" ||
-            //     __instance.def.thingClass.Name == "RimFridge_Building")
-            // {
-            //     return;
-            // }
-            
             // Short-circuit the base GetGizmos patch if it'll be called later-on
             if (customGetGizmos.Contains(__instance.GetType()) &&
                 __originalMethod.DeclaringType == typeof(Building_Storage))
             {
-                LogNotPatching(__instance, "GetGizmos will be overridden");
+                LogOnlyOnce(__instance.GetType(), false, "GetGizmos will be overridden");
                 return;
             }
             
@@ -100,7 +92,7 @@ namespace ShelfRenamer
                         command.defaultLabel == "Rename".Translate())
                     {
                         // Log to the console that we're not patching this,
-                        LogNotPatching(__instance, "has gizmo already");
+                        LogOnlyOnce(__instance.GetType(), false, "has gizmo already");
                         // and skip adding a gizmo
                         return;
                     }
@@ -111,10 +103,11 @@ namespace ShelfRenamer
             // If it has a user-accessible storage tab, then allow renaming.
             if (!__instance.StorageTabVisible)
             {
-                LogNotPatching(__instance, "no storage tab");
+                LogOnlyOnce(__instance.GetType(), false, "no storage tab");
                 return;
             }
 
+            LogOnlyOnce(__instance.GetType(), true, $"OK for {__instance.GetType()}");
             var tempList = __result.ToList();
             // Add our own rename button, since none appears to exist already.
             tempList.Add(new Command_Action
